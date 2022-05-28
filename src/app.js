859aeee47daee455165577271b4758278c371bfc
Shiv10 <shivanshsharma2012@gmail.com>
2022-05-28 22:44:55 +0530

    feat: init setup

---
+++ src/app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const rateLimiter = require('express-rate-limit');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//limter will limit our number of requests to 100 requests per minute
+const limiter = rateLimiter({
+	windowMs: 60*1000,
+	max: 100,
+	standardHeaders: true,
+	legacyHeaders: false
+});
+
+app.use(limiter);
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST']
+}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => {
+	res.send("test");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
