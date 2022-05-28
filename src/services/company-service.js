f9ab763e170702aed69e31283a69f6fa61231352
Shiv10 <shivanshsharma2012@gmail.com>
2022-05-29 01:31:47 +0530

    fix: remove redis

--- src/services/company-service.js
+++ src/services/company-service.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const randomstring = require('randomstring');
-const Redis = require('redis');
-const redisClient = Redis.createClient()
+const sessionstorage = require('sessionstorage');
 const company = require('../models/company');
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID
@@ -10,7 +9,6 @@ const authToken = process.env.TWILIO_ACCOUNT_AUTHKEY
 const client = require('twilio')(accountSid, authToken);
 
 const SMS_TEMPLATE = 'The OTP token for your Feit Account is <token>. Please enter the OTP to create your account';
-const OTP_ALIVE_TIME = 15 * 60;
 
 const formatMessage = (template, otp) => {
     let result = template.replace('<token>', otp);
@@ -25,18 +23,18 @@ router.post('/sendOtp', async (req, res) => {
             return res.status(403).json({success: false, message: 'Same number exists in DB'});
         }
         const mobileNumber = `${countryCode}${phone}`
-        let currentOtpRetries = await redisClient.hGet(`retries-${phone}`, 'otpRetries');
+        let currentOtpRetries =  sessionstorage.getItem(`retries-${phone}`);
         if (currentOtpRetries != null) {
             // I am converting it to number here because one of the output is null and I don't want to convert null to number
             currentOtpRetries = +currentOtpRetries;
             if (currentOtpRetries > 2) {
-              await redisClient.del(phone);
-              await redisClient.del(`retries-${phone}`);
+              sessionstorage.removeItem(phone);
+              sessionstorage.removeItem(`retries-${phone}`);
               return res.status(403).json({success: false, message: 'Max OTP retries reached'})
             }
 
-            await redisClient.hIncrBy(`retries-${phone}`, 'otpRetries', 1);
-            const otp = await redisClient.hGet(phone, 'otp');
+            sessionstorage.setItem(`retries-${phone}`, currentOtpRetries+1);
+            const otp = sessionstorage.getItem(phone);
             const otpMessage = formatMessage(SMS_TEMPLATE, otp );
 
             let msg = await client.messages.create({
@@ -54,10 +52,8 @@ router.post('/sendOtp', async (req, res) => {
         });
 
         const otpMessage = formatMessage( SMS_TEMPLATE, otp );
-        await redisClient.hSet(phone, 'otp', otp);
-        await redisClient.hSet(`retries-${phone}`, 'otpRetries', 0);
-        await redisClient.expire(phone, OTP_ALIVE_TIME);
-        await redisClient.expire(`retries-${phone}`, OTP_ALIVE_TIME);
+        sessionstorage.setItem(phone, otp);
+        sessionstorage.setItem(`retries-${phone}`, 0);
         let msg = await client.messages.create({
             body: otpMessage,
             from: process.env.TWILIO_ACCOUNT_NUMBER,
@@ -66,6 +62,14 @@ router.post('/sendOtp', async (req, res) => {
         console.log(msg.sid);
         return res.send(200).json({success: true, message: 'OTP sent'});
 
+    } catch (e) {
+        return res.status(500).json({success: false, message: 'Some error occurred'});
+    }
+});
+
+router.post('/signup', async (req, res) => {
+    try {
+        const {phone, companyName, typeOfBusiness, employeeName, address, otp} = req.body; 
     } catch (e) {
         return res.status(500).json({success: false, message: 'Some error occurred'});
     }
