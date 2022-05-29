67d25c0339d6f872eb30638c3cc7854863083080
Shiv10 <shivanshsharma2012@gmail.com>
2022-05-29 13:06:32 +0530

    feat: add produxt registration

--- src/services/client-service.js
+++ src/services/client-service.js
@@ -5,6 +5,25 @@ const sessionstorage = require('sessionstorage');
 const user = require('../models/user');
 const hashPassword = require('../tools/hash');
 const jwt = require('jsonwebtoken');
+const {
+	AccountId,
+	PrivateKey,
+	Client,
+	FileCreateTransaction,
+	ContractCreateTransaction,
+	ContractFunctionParameters,
+	ContractExecuteTransaction,
+	ContractCallQuery,
+	Hbar,
+} = require("@hashgraph/sdk");
+const authorize = require('../middlewares/auth');
+
+// Configure accounts and client
+const operatorId = AccountId.fromString(process.env.ACCOUNT_ID);
+const operatorKey = PrivateKey.fromString(process.env.PRIVATE_KEY);
+
+const hederaClient = Client.forTestnet().setOperator(operatorId, operatorKey);
+const contractId = '0.0.34934008'
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_ACCOUNT_AUTHKEY
@@ -117,6 +136,40 @@ router.post('/login', async (req, res) => {
         }
     
         return res.status(401).json({ success: false, error: "Invalid credentials" });
+    } catch (e) {
+        return res.status(500).json({success: false, message: 'Internal server error '});
+    }
+});
+
+router.post('/registerProduct', authorize, async (req, res) => {
+    try {
+
+        const {serialNumber, phone} = req.body;
+        phone = parseInt(phone);
+        serialNumber = parseInt(serialNumber);
+    
+        const contractQueryTx = new ContractCallQuery()
+            .setContractId(contractId)
+            .setGas(100000)
+            .setFunction("getProduct", new ContractFunctionParameters().addUint256(serialNumber));
+        const contractQuerySubmit = await contractQueryTx.execute(client);
+        const contractQueryResult = contractQuerySubmit.getUint256(0);
+        
+        if(contractQueryResult.toString().length==10) {
+            return res.status(403).json({success: false, message: 'Product already registered.'});
+        }
+    
+        const contractExecuteTx = new ContractExecuteTransaction()
+            .setContractId(contractId)
+            .setGas(100000)
+            .setFunction("setProduct", new ContractFunctionParameters().addUint256(serialNumber).addUint256(phone));
+    
+        const contractExecuteSubmit = await contractExecuteTx.execute(client);
+        const contractExecuteRx = await contractExecuteSubmit.getReceipt(client);
+        if(contractExecuteRx.status =='SUCCESS') {
+            return res.status(200).json({success: true, message: 'Product registered successfully.'})
+        }
+        return res.status(500).json({success: false, message: 'Internal server error '});
     } catch (e) {
         return res.status(500).json({success: false, message: 'Internal server error '});
     }
